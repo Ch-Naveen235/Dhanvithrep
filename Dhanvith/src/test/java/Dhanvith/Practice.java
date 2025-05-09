0d2668cc885bec0752e1ab3ead4eac6e2254563f
DELL <DELL@DESKTOP-APHTPVC>
2025-05-09 10:11:55 +0530

    build created

--- Dhanvith/src/test/java/Dhanvith/Practice.java
+++ Dhanvith/src/test/java/Dhanvith/Practice.java
@@ -3,13 +3,15 @@ package Dhanvith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class Practice {
-public static void main(String[] args) {
-	
-		WebDriver driver=new ChromeDriver();
+	WebDriver driver;
+@Test()
+	public void test() {
+		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.saucedemo.com/");
 		driver.findElement(By.xpath("(//input[@class='input_error form_input'])[1]")).sendKeys("standard_user");
@@ -17,6 +19,15 @@ public static void main(String[] args) {
 		driver.findElement(By.xpath("//input[@id=\"login-button\"]")).click();
 		driver.quit();
 }
-	
+	@Test
+	public void m1() {
+	driver=new FirefoxDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.saucedemo.com/");
+	driver.findElement(By.xpath("(//input[@class='input_error form_input'])[1]")).sendKeys("standard_user");
+	driver.findElement(By.xpath("(//input[@class=\"input_error form_input\"])[2]")).sendKeys("secret_sauce");
+	driver.findElement(By.xpath("//input[@id=\"login-button\"]")).click();
+	driver.quit();
+	}
 }
 
